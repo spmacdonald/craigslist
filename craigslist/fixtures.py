16ffb5738bc5ea5fe403c9153e59c7a19426b9ec
Andrew Brookins <a.m.brookins@gmail.com>
2012-05-28 13:47:44 -0700

    Add test fixtures

---
+++ craigslist/fixtures.py
@@ -0,0 +1,18 @@
+for_sale = [
+    """
+    <h4 class="ban">Mon May 28</h4>
+    		<p class="row">
+			<span class="ih">&nbsp;</span>
+			<a href="http://seattle.craigslist.org/tac/ppd/2992142629.html">SUPER DRYER SALE FROM $99 ON UP </a> -
+			<font size="-1"> (PUYALLUP)</font> <span class="p"> img</span> <small class="gc"><a href="/ppd/">appliances - by dealer</a></small><br class="c">
+		</p>
+    """,
+    """
+    <h4 class="ban">Mon May 28</h4>
+    		<p class="row">
+			<span class="ih">&nbsp;</span>
+			<a href="http://seattle.craigslist.org/tac/ppd/3007461942.html">SAMSUNG STAINLESS DISHWASHERS (NEW)</a> -
+			 $599<font size="-1"> (PUYALLUP)</font> <span class="p"> img</span> <small class="gc"><a href="/ppd/">appliances - by dealer</a></small><br class="c">
+		</p>
+    """
+]
