1e65a697280237a51782a7f15964d3fe671b1999
Andrew Brookins <a.m.brookins@gmail.com>
2012-05-28 09:13:05 -0700

    Package for distribution

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='craigslist',
+    description='A Python API for searching Craigslist.',
+    long_description=read('README'),
+    author='Andrew Brookins',
+    author_email='a@andrewbrookins.com',
+    url='https://github.com/abrookins/craigslist',
+    version='0.1',
+    packages=['craigslist'],
+    install_requires=[
+        'BeautifulSoup==3.2.0',
+        'requests==0.10.1'
+    ]
+)
